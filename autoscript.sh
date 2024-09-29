e0651f50d3a17a9a6d2159d3a896c61b558fc316
NaW <123004876+NaW5@users.noreply.github.com>
2024-09-30 06:29:08 +0700

    update

--- autoscript.sh
+++ autoscript.sh
@@ -24,9 +24,9 @@ ifcfg="/etc/sysconfig/network-scripts/ifcfg-${interface}"
 named="/etc/named.conf"
 namedrfc="/etc/named.rfc1912.zones"
 # Backup file.
-cp $ifcfg "${ifcfg}.bak"
-cp $named "${named}.bak"
-cp $namedrfc "${namedrfc}.bak"
+cp "$ifcfg" "${ifcfg}.bak"
+cp "$named" "${named}.bak"
+cp "$namedrfc" "${namedrfc}.bak"
 trap cleanup SIGINT
 # Tắt firewall
 firewall-cmd --permanent --zone=public --add-service=dns > /dev/null 2>&1
@@ -37,9 +37,9 @@ cleanup()
 {
 
 	if [[ -f "${ifcfg}.bak" && -f "${named}.bak" && -f "${namedrfc}.bak" ]]; then
-		mv "${ifcfg}.bak" $ifcfg
-		mv "${named}.bak" $named
-		mv "${namedrfc}.bak" $namedrfc
+		mv "${ifcfg}.bak" "$ifcfg"
+		mv "${named}.bak" "$named"
+		mv "${namedrfc}.bak" "$namedrfc"
 	fi
 	systemctl restart network
 	systemctl restart named
@@ -56,17 +56,17 @@ prompt()
 		echo "4. Cấu hình forward DNS."
 		echo "0. Thoát"
 		read -p "Lựa chọn: " choice
-		case $choice in
+		case "$choice" in
 			1)
 				while true; do
 					echo "Chọn chế độ muốn cấu hình: "
 					echo "1. Tĩnh."
 					echo "2. Động."
 					read -p "Lựa chọn: " choice
-					if [[ $choice == 1 ]]; then
+					if [[ "$choice" == 1 ]]; then
 						ip_setup 1
 						break
-					elif [[ $choice == 2 ]]; then
+					elif [[ "$choice" == 2 ]]; then
 						ip_setup
 						break
 					else 
@@ -83,10 +83,10 @@ prompt()
 					echo "1. Primary"
 					echo "2. Backup"
 					read -p "Lựa chọn: " choice
-					if [[ $choice == 1 ]]; then
+					if [[ "$choice" == 1 ]]; then
 						backup_setup 2
 						break
-					elif [[ $choice == 2 ]]; then
+					elif [[ "$choice" == 2 ]]; then
 						backup_setup 3
 						break
 					else
@@ -111,7 +111,7 @@ prompt()
 ip_setup() 
 {
 	local mode="$1"
-	if [[ $mode == 1 ]]; then
+	if [[ "$mode" == 1 ]]; then
 		while true; do
 			read -p "Nhập địa chỉ IP/DNS cho hệ thống: " ipaddr
 			if ! ip_check "$ipaddr"; then
@@ -132,13 +132,13 @@ ip_setup()
 		} >> "$ifcfg"
 		
 		if ! grep -q "listen-on .*${ipaddr};" "$named"; then
-			sed -i -e "/listen-on .*127\.0\.0\.1;/s|;|; ${ipaddr};| " $named
+			sed -i -e "/listen-on .*127\.0\.0\.1;/s|;|; ${ipaddr};| " "$named"
 		fi
 		if ! grep -q "#listen-on-v6" "$named"; then
-			sed -i -e "s/listen-on-v6/#listen-on-v6/" $named
+			sed -i -e "s/listen-on-v6/#listen-on-v6/" "$named"
 		fi
 		if ! grep -q "allow-query.*any;" "$named"; then
-			sed -i -e "/allow-query.*localhost;/s|;|; any;|" $named
+			sed -i -e "/allow-query.*localhost;/s|;|; any;|" "$named"
 		fi
 	else
 		sed -i -e "s/^BOOTPROTO=.*$/BOOTPROTO=dhcp/" \
@@ -158,7 +158,7 @@ fw_zone() {
 		return
 	fi
 	fw_rec="forward.${fw_dom}"
-	if [[ $mode != 3 ]]; then
+	if [[ "$mode" != 3 ]]; then
 		while true; do
 			read -p "Nhập địa chỉ cho ${fw_dom} : " zone_ip
 			if ! ip_check "$zone_ip"; then
@@ -168,7 +168,7 @@ fw_zone() {
 			fi
 		done
 	else
-		zone_ip=$ipaddr
+		zone_ip="$ipaddr"
 	fi
 	zone_prefix=$(ip_to_prefix_length "$zone_ip")
 	{
@@ -181,7 +181,7 @@ fw_zone() {
 	} >> "$namedrfc"
 	
 	# Tạo record forward
-	if [[ $mode != 3 ]]; then
+	if [[ "$mode" != 3 ]]; then
 		name_server="server"
 		admin_server="root"
 		
@@ -215,23 +215,24 @@ fw_zone() {
 		reverse_zone="${octets[3]}.${octets[2]}.${octets[1]}.${octets[0]}.in-addr.arpa"
 	fi
 	
-	rev_zone "$mode" "$reverse_zone" "$zone_prefix" "$zone_ip" "$name_server" "$admin_server"
-	systemctl restart network
+	rev_zone "$mode" "$fw_dom" "$reverse_zone" "$zone_prefix" "$zone_ip" "$name_server" "$admin_server"
+	
 	systemctl start named > /dev/null 2>&1
 	systemctl restart named > /dev/null 2>&1
 }
 
 rev_zone() {
 	local mode="$1"
-	local re_zone="$2"
-	local zone_prefix="$3"
-	local zone_ip="$4"
-	local name_server="$5"
-	local admin_server="$6"
-	local backup_exclusive="$7"
+	local fw_dom="$2"
+	local re_zone="$3"
+	local zone_prefix="$4"
+	local zone_ip="$5"
+	local name_server="$6"
+	local admin_server="$7"
+	local backup_exclusive="$8"
 		
+	re_rec="rev.$(ipcalc -n "${zone_ip}/${zone_prefix}" | awk -F'=' '{print $2}')"
 	if ! grep -qE "zone[[:space:]]*\"${re_zone}\"" "$namedrfc"; then
-		re_rec="rev.$(ipcalc -n "${zone_ip}/${zone_prefix}" | awk -F'=' '{print $2}')"
 		{
 			echo "zone \"${re_zone}\"	IN	{"
 			echo "		type master;"
@@ -243,7 +244,7 @@ rev_zone() {
 	fi
 	
 	# Tạo record reverse 
-	if [[ $mode != 3 ]]; then
+	if [[ "$mode" != 3 ]]; then
 		{
 			if ! grep -q "^\$TTL" "/var/named/${re_rec}" > /dev/null 2>&1; then
 				echo "\$TTL 86400"
@@ -277,21 +278,18 @@ rev_zone() {
 			fi
 		} >> "/var/named/${re_rec}"
 	fi
-	systemctl restart network
-	systemctl start named > /dev/null 2>&1
-	systemctl restart named > /dev/null 2>&1
 }
 
 zone_setup() 
 {
 	local type="$1"
-	if [[ $type == 3 ]]; then
+	if [[ "$type" == 3 ]]; then
 		echo "Nhập các zone tương ứng với primary server."
 	fi
 	while true; do
-		fw_zone $type
+		fw_zone "$type"
 		read -p "Tiếp tục khai báo zone ? (y/n): " choice
-		if [[ $choice != "y" ]]; then
+		if [[ "$choice" != "y" ]]; then
 			break
 		fi
 	done
@@ -300,17 +298,21 @@ zone_setup()
 backup_setup()
 {
 	local type="$1"
-	if ! grep -q "BOOTPROTO=none" $ifcfg; then
+	if ! grep -q "BOOTPROTO=none" "$ifcfg"; then
 		echo "Cấu hình IP tĩnh."
 		ip_setup 1
-		zone_setup $type
 	fi
 	
 	while true; do
-		if [[ $type == 2 ]]; then
+		if [[ "$type" == 2 ]]; then
 			read -p "Nhập địa chỉ máy chủ backup: " trans_ip
-		elif [[ $type == 3 ]]; then
+		elif [[ "$type" == 3 ]]; then
 			read -p "Nhập địa chỉ máy chủ primary: " trans_ip
+			if [ ! -f ~/.ssh/id_rsa ]; then
+		    	ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -q -N "" > /dev/null 2>&1
+			fi
+			ssh-copy-id root@"$trans_ip" > /dev/null 2>&1
+			scp root@"$trans_ip":/etc/named.rfc1912.zones /etc/ > /dev/null 2>&1
 		fi
 
 		if ! ip_check "$trans_ip"; then
@@ -319,37 +321,82 @@ backup_setup()
 			break
 		fi
 	done
+	
+	zones=$(get_zones)
 
-	if [[ $type == 3 ]]; then
-		
-		if [ ! -f ~/.ssh/id_rsa ]; then
-	    	ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -q -N "" > /dev/null 2>&1
+	if [[ "$type" == 2 ]]; then
+		local backup_prefix=$(ip_to_prefix_length "$trans_ip")
+		local -a octets
+		IFS='.' read -r -a octets <<< "$trans_ip"
+		local reverse_backup
+
+		if [[ "$backup_prefix" -le 8 ]]; then
+			reverse_backup="${octets[0]}.in-addr.arpa"
+		elif [[ "$backup_prefix" -le 16 ]]; then
+			reverse_backup="${octets[1]}.${octets[0]}.in-addr.arpa"
+		elif [[ "$backup_prefix" -le 24 ]]; then
+			reverse_backup="${octets[2]}.${octets[1]}.${octets[0]}.in-addr.arpa"
+		else
+			reverse_backup="${octets[3]}.${octets[2]}.${octets[1]}.${octets[0]}.in-addr.arpa"
 		fi
-		ssh-copy-id root@"$trans_ip" > /dev/null 2>&1
-		scp root@"$trans_ip":/etc/named.rfc1912.zones /etc/named.rfc1912.zones > /dev/null 2>&1
-		zones=$(get_zones)
-		pattern="/zone.*${reverse_backup}/,/^}/"
-		sed -i -e "${pattern} {s|type .*;|type slave;|}" \
-				-e "${pattern} {s|file[^\"]*\"|file \"slaves/|}" \
-				-e "${pattern} {s|allow-update .*};|masters {${trans_ip};};|}" $namedrfc	
+		
+		local primary_holder=(echo ${numbers[1]})
+		rev_zone "2" "$primary_holder" "$reverse_backup" "$backup_prefix" "$trans_ip" "backup" "admin_backup" "0"
 	fi
 	
 	
-	if grep -q 'allow-transfer' $named; then
-		sed -i  "/allow-transfer .*localhost;/s|;|; ${trans_ip};|" $named
+	if grep -q 'allow-transfer' "$named"; then
+		if ! grep -q "allow-transfer.*${trans_ip}" "$named"; then
+			sed -i  "/allow-transfer .*localhost;/s|;|; ${trans_ip};|" "$named"
+		fi
 	else
-		sed -i "/allow-query.*};/s|};|};\n\tallow-transfer { localhost; ${trans_ip}; };\n|" $named
+		sed -i "/allow-query.*};/s|};|};\n\tallow-transfer { localhost; ${trans_ip}; };\n|" "$named"
 	fi
-	systemctl start named > /dev/null 2>&1
+
+	for backup_zone in $zones; do
+		if [[ "$type" == 2 ]]; then
+			if [[ ! $backup_zone == *"in-addr.arpa" ]]; then
+
+				zone_rec=$(zone_file "$backup_zone" "$namedrfc")
+				re_rec="rev.$(ipcalc -n "${trans_ip}/${backup_prefix}" | awk -F'=' '{print $2}')"
+
+				if ! grep -q "backup.${backup_zone}." "/var/named/${zone_rec}"; then
+					{
+						echo "@	IN	NS		backup.${backup_zone}."
+						echo "backup	IN	A		${trans_ip}"
+					} >> "/var/named/${zone_rec}"
+				fi
+
+				local reverse_ip="${octets[3]}.${octets[2]}.${octets[1]}.${octets[0]}"
+				local extracted_ip="${reverse_backup//.in-addr.arpa/}"
+
+	            if ! grep -q "${reverse_ip//.${extracted_ip}}.*backup.${backup_zone}" "/var/named/${re_rec}"; then
+					{
+						echo "@	IN	NS	backup.${backup_zone}."
+						echo "${reverse_ip//.${extracted_ip}}	IN	PTR	backup.${backup_zone}."
+
+					} >> "/var/named/${re_rec}"
+				fi
+				
+			fi
+		elif [[ "$type" == 3 ]]; then
+			pattern="/zone.*${backup_zone}/,/^}/"
+			if ! sed -n "${pattern}p" "$namedrfc" | grep -q "type slave;"; then
+				sed -i -e "${pattern} {s|type .*;|type slave;|}" \
+						-e "${pattern} {s|file[^\"]*\"|file \"slaves/|}" \
+						-e "${pattern} {s|allow-update .*};|masters {${trans_ip};};|}" "$namedrfc"
+			fi
+		fi
+	done
+	
 	systemctl restart named > /dev/null 2>&1
 }
 
 forwarder_setup()
 {
-	if ! grep -q "BOOTPROTO=none" $ifcfg; then
+	if ! grep -q "BOOTPROTO=none" "$ifcfg"; then
 		echo "Cấu hình IP, DNS."
-		ip_setup
-		zone_setup
+		ip_setup 1
 	fi
 	while true; do
 		read -p "Nhập địa chỉ máy chủ muốn chuyển tiếp truy vấn: " forward_ip
@@ -359,13 +406,15 @@ forwarder_setup()
 			break
 		fi
 	done
-	if grep -q 'forwarders' $named; then
-		sed -i  "/forwarders .*{/s|{|{ ${forward_ip};|" $named
+	if grep -q 'forwarders' "$named"; then
+		if ! grep -q "forwarders.*${forward_ip}" "$named"; then
+			sed -i  "/forwarders .*{/s|{|{ ${forward_ip};|" "$named"
+		fi
 	else
-		sed -i "/allow-query.*};/s|};|};\n\tforwarders { ${forward_ip}; };\n|" $named
+		sed -i "/allow-query.*};/s|};|};\n\tforwarders { ${forward_ip}; };\n|" "$named"
 	fi
 	sed -i -e "s/dnssec-enable.*;/dnssec-enable no;/" \
-			-e "s/dnssec-validation.*;/dnssec-validation no;/" $named
+			-e "s/dnssec-validation.*;/dnssec-validation no;/" "$named"
 	sed -i -e "s/SELINUX=.*/SELINUX=disabled/" "/etc/sysconfig/selinux"
 }
 zone_file()
@@ -388,12 +437,13 @@ zone_file()
 			in_zone = 0
 		}
 	' "$file")
-	echo $filename
+	
+	echo "$filename"
 }
 is_default_and_rev_zone() 
 {
     local zone="$1"
-	local exclude="arpa|localhost"
+	local exclude="0\.in-addr\.arpa|1\.0\.0\.127|ip6\.arpa|localhost"
     if [[ "$zone" =~ $exclude ]]; then
         return 0
     fi
@@ -408,8 +458,8 @@ get_zones()
         if [[ "$line" =~ ^zone ]]; then
             
             zone_do=$(echo "$line" | awk -F '"' '{print $2}')
-            if ! is_default_and_rev_zone $zone_do; then
-                zones+=($zone_do) 
+            if ! is_default_and_rev_zone "$zone_do"; then
+                zones+=("$zone_do") 
             fi
         fi
     done < "$namedrfc"
@@ -418,7 +468,7 @@ get_zones()
 }
 ip_to_prefix_length() 
 {
-    ip=$1
+    ip="$1"
     IFS='.' read -r i1 i2 i3 i4 <<< "$ip"
     
     if (( i1 >= 1 && i1 <= 126 )); then
@@ -448,4 +498,3 @@ ip_check()
 }
 
 prompt
-nmcli radio wifi on
